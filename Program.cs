f2dc44d27366c20361a7199e064802b29f48fcf8
Necip Dirik <necipdirik@gmail.com>
2025-03-14 11:58:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CSharpDelegates
+{
+    public static class CoffeeMachine
+    {
+        public static void MakeCoffee(string coffeeName)
+        {
+            Console.WriteLine($"Making {coffeeName} coffee");
+        }
+    }
+    static class Program
+    {
+        // delegate void CoffeeDelegate(string coffeeName);
+
+        // Delegate yerine Action kullanıyoruz
+        static Action<string> coffeeAction = CoffeeMachine.MakeCoffee;
+
+        static void Main(string[] args)
+        {
+            // CoffeeDelegate coffeeDelegate = new CoffeeDelegate(CoffeeMachine.MakeCoffee);
+
+            Dictionary<int, string> coffees = new()
+            {
+                { 1, "Espresso" },
+                { 2, "Latte" },
+                { 3, "Cappuccino" }
+            };
+
+            Console.WriteLine("Select your coffee:");
+            Console.WriteLine("-------------------");
+
+            foreach (var option in coffees)
+            {
+                Console.WriteLine($"{option.Key} - {option.Value}");
+            }
+
+            Console.WriteLine("-------------------");
+
+
+            if (int.TryParse(Console.ReadLine(), out int selection) && coffees.ContainsKey(selection))
+            {
+                coffeeAction(coffees[selection]);
+            }
+            else
+            {
+                Console.WriteLine("🚫 Geçersiz seçim!");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Scenario:
+
+Delegate in C#
+
+## Scenario Description:
+
+- This project creates a Coffee Machine that can make different types of coffee using the following delegate methods:
+
+	- Custom Delegate,
+	- Action Delegate,
+	- Func Delegate.
+
+## Features:
+
+- Delegates in C# are similar to function pointers in C or C++.
+- A delegate is a reference type variable that holds the reference to a method.
+- The reference can be changed at runtime.
+- Delegates are especially used for implementing events and the call-back methods.
+- All delegates are implicitly derived from the System.Delegate class.
